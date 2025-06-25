78eb87ad968216778bf3bcbf7ab34700fe4001e9
vinod kumar <vinod783058@example.com>
2025-06-25 15:06:25 +0530

    newcomits

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://exam-seating-zutu.vercel.app/', 'http://localhost:3000', 'http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
