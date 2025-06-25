d04d753684cdd683450282e3ce7888ce29fb9640
vinod kumar <vinod783058@example.com>
2025-06-25 16:05:08 +0530

    namechange

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 const allowedOrigins = [
-  'https://exam-seating-cgy3.vercel.app', // your frontend URL
+  'https://exam-seating-management.vercel.app', // your frontend URL
 ];
 
 app.use(cors({
